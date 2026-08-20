01ffdff9da3e0cb7e07150c3aa3ba0b357e33a5b
Waleed Siddiqui <waleedsiddiqui.py@gmail.com>
2026-08-20 20:20:53 +0500

    done chart

--- src/components/Card-2.jsx
+++ src/components/Card-2.jsx
@@ -5,22 +5,22 @@ import {
 
 
 export function CardDemo({ src, title, packs, tags, variant }) {
-    console.log("waleed")
+
     console.log(src, title, packs, tags, "data")
     return (
         <>
             {variant === "Midi" ?
-                <Card Card className=" h-65 w-[140px] mb-8 mt-2  rounded-none p-0" >
-                    <div className="flex gap-5 ">
+                <Card Card className=" h-65 w-[140px] mb-8 mt-2   p-0" >
+                    <div className="flex gap-5   ">
                         <div className="flex flex-col ">
                             <div>
-                                <img src={src} alt={title} width={55} className="   w-35" />
+                                <img src={src} alt={title} width={55} className=" rounded-sm h-40 w-45" />
                             </div>
 
                             <div className="flex flex-col gap-3">
-                                <div className="flex flex-col gap-3 mt-2 px-2">
-                                    <h1 className="text-black font-semibold">{title}</h1>
-                                    <p className="text-[#A9A9A9]">{packs}</p>
+                                <div className="flex flex-col gap-0  mt-2 px-2">
+                                    <h1 className="text-black text-[13px] font-bold">{title}</h1>
+                                    <p className="text-[#A9A9A9] text-[10px]">{packs}</p>
                                 </div>
                                 <div className="flex gap-3 text-[#A9A9A9] px-2">
                                     {tags}
--- src/pages/Charts.jsx
+++ src/pages/Charts.jsx
@@ -1,7 +1,301 @@
 import React from 'react'
 import NavBar from '@/components/NavHome'
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import PackCard from '@/components/PackCard'
 export default function Charts() {
+    const cardData = [
+        {
+            src: "/src/assets/p1.png",
+            id: "1",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "2",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+            ,
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "3",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "4",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p1.png",
+            id: "5",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "6",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p1.png",
+            id: "7",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "8",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "9",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p1.png",
+            id: "10",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "11",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "12",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "13",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "14",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "15",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "16",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "17",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "18",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "19",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "20",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "21",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "22",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "23",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "24",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "25",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "26",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "27",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "28",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "29",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "30",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "31",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "32",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+        {
+            src: "/src/assets/p3.png",
+            id: "33",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585"
+        },
+        {
+            src: "/src/assets/p2.png",
+            id: "34",
+            title: "Tape & Vinyl Drums",
+            description: "by sample Magic",
+            img: "/src/assets/z2.png",
+            num: "585",
+            showArrow: "/src/assets/arror.png"
+        },
+    ]
     return (
         <div>
             <NavBar />
@@ -9,18 +303,71 @@ export default function Charts() {
                 <div className='flex flex-col gap-10'>
                     <h1 className='text-black font-extrabold cursor-pointer '>Sounds</h1>
                     <div>
-                        <Link><h1 className='text-black font-bold cursor-pointer'>Browse</h1></Link>
-                        <Link to="/Charts"><h1 className='text-black cursor-pointer'>Charts</h1></Link>
+                        <NavLink to="/sound">
+                            {({ isActive }) => (
+                                <h1 className={`text-black cursor-pointer ${isActive ? 'font-bold' : ''}`}>
+                                    Browse
+                                </h1>
+                            )}
+                        </NavLink>
+
+                        <NavLink to="/Charts">
+                            {({ isActive }) => (
+                                <h1 className={`text-black cursor-pointer ${isActive ? 'font-bold' : ''}`}>
+                                    Charts
+                                </h1>
+                            )}
+                        </NavLink>
                     </div>
                     <div>
                         <h1 className='text-black '>Your Library</h1>
-                        <h1 className='text-black cursor-pointer'>Sounds</h1>
+                        <NavLink to="/Sound2">
+                            {({ isActive }) => (
+                                <h1 className={`text-black cursor-pointer ${isActive ? 'font-bold' : ''}`}>
+                                   Sound
+                                </h1>
+                            )}
+                        </NavLink>
                     </div>
                 </div>
-                <img src="src/assets/line.png" alt="" className='h-950' />
+                <img src="src/assets/line.png" alt="" className='h-700' />
                 <div className='flex flex-col gap-4'>
                     <h1 className=' text-red-400  font-bold ml-60'>You are currently viewing company sounds</h1>
                     <hr className='w-240 mr-0' />
+                    <div className='flex gap-10'>
+                        <NavLink to="/Charts">
+                            {({ isActive }) => (
+                                <h1 className={`text-black cursor-pointer ${isActive ? 'font-bold' : ''}`}>
+                                    Weekly top packs
+                                </h1>
+                            )}
+                        </NavLink>
+
+                        <NavLink to="/top labels">
+                            {({ isActive }) => (
+                                <h1 className={`text-black cursor-pointer ${isActive ? 'font-bold' : ''}`}>
+                                    Weekly top labels
+                                </h1>
+                            )}
+                        </NavLink>
+                    </div>
+                    <div>
+                        {cardData.map((data, index) => (
+                            <PackCard
+                                key={index}
+                                id={data.id}
+                                src={data.src}
+                                title={data.title}
+                                description={data.description}
+                                img={data.img}
+                                num={data.num}
+                                ChangeRow={index % 2 === 1}
+                                showArrow={data.showArrow}
+
+                            />
+                        ))}
+
+                    </div>
                 </div>
 
             </div>
